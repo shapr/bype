acd1d21daa6b96893545780cb0826bd0a083a76e
Jaseem Abid <me@jabid.in>
2024-07-03 11:07:55 +0100

    Deterministic compression and tests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     fmt::{self, Debug},
 };
 
-#[derive(Eq, Clone, Copy, Hash, PartialEq)]
+#[derive(Eq, Ord, Clone, Copy, Hash, PartialEq, PartialOrd)]
 struct Pair(u8, u8);
 
 type Count = u32;
@@ -35,26 +35,22 @@ impl CountsTable {
 
     /// Most frequent pair
     fn top_pair(&self) -> Option<Pair> {
-        self.0
-            .iter()
+        // Sort for this to be deterministic
+        let mut counts = self.0.iter().collect::<Vec<_>>();
+        counts.sort_by(|a, b| a.0.cmp(b.0));
+        counts
+            .into_iter()
             .max_by(|a, b| a.1.cmp(b.1))
             .and_then(|(pair, count)| if *count > 1 { Some(*pair) } else { None })
-    }
-
-    /// Decrease the count associated with a pair
-    fn decrease(&mut self, pair: &Pair, count: Count) {
-        println!("Decrease {:?} by {}", pair, count);
-        self.0.entry(*pair).and_modify(|counter| *counter -= count);
     }
 }
 
 /// Replace `replacement` in `source` with `id`
 ///
 /// return (new buffer, number of replacements)
-fn replace(source: &[u8], replacement: Pair, id: u8) -> (Vec<u8>, Count) {
+fn replace(source: &[u8], replacement: Pair, id: u8) -> Vec<u8> {
     let mut buffer = Vec::<u8>::new();
     let mut skip_next = false;
-    let mut replacements = 0;
 
     for pair in std::iter::zip(source.iter(), source[1..].iter()) {
         if skip_next {
@@ -63,7 +59,6 @@ fn replace(source: &[u8], replacement: Pair, id: u8) -> (Vec<u8>, Count) {
         }
         if Pair(*pair.0, *pair.1) == replacement {
             buffer.push(id);
-            replacements += 1;
             skip_next = true;
         } else {
             buffer.push(*pair.0);
@@ -73,29 +68,27 @@ fn replace(source: &[u8], replacement: Pair, id: u8) -> (Vec<u8>, Count) {
         buffer.push(*source.last().unwrap());
     }
 
-    (buffer, replacements)
+    buffer
 }
 
 fn compress(input: &str) -> (Vec<u8>, Vec<(Pair, u8)>) {
     let mut bytes: Vec<u8> = input.as_bytes().into();
-    let mut count;
     let mut table: Vec<(Pair, u8)> = Vec::new(); // A list of Replacements and Counts.
-    let mut counts = CountsTable::new(&bytes); // Frequency of each pair
 
-    while let Some(candidate_pair) = counts.top_pair() {
+    // In theory, the table needs to be computed only once but that's work.
+    while let Some(candidate_pair) = CountsTable::new(&bytes).top_pair() {
         if let Some(unused_byte) = get_unused_byte(&bytes) {
-            println!("Before step {:?}", std::str::from_utf8(&bytes).unwrap());
+            // println!("Before step {:?}", std::str::from_utf8(&bytes).unwrap());
             println!(
-                "Replace '{candidate_pair:?}' -> {:?}",
+                "Replace {candidate_pair:?} -> {:?}",
                 char::from(unused_byte)
             );
 
             table.push((candidate_pair, unused_byte));
 
-            (bytes, count) = replace(&bytes, candidate_pair, unused_byte);
+            bytes = replace(&bytes, candidate_pair, unused_byte);
 
-            counts.decrease(&candidate_pair, count);
-            println!("After step {:?}\n", std::str::from_utf8(&bytes).unwrap());
+            // println!("After step {:?}\n", std::str::from_utf8(&bytes).unwrap());
         }
     }
 
@@ -103,7 +96,7 @@ fn compress(input: &str) -> (Vec<u8>, Vec<(Pair, u8)>) {
 }
 
 fn main() {
-    let input = "Hel Hello World AAA XYZ"; // include_str!("main.rs");
+    let input = "He Hello World AAA XYZ"; // include_str!("main.rs");
     let (output, table) = compress(input);
 
     println!("Output = {:?}", std::str::from_utf8(&output).unwrap());
@@ -114,7 +107,7 @@ fn main() {
     println!("\nReplacement Table: ");
 
     for (pair, replacement) in table {
-        println!("Replace '{:?}' -> {:?} ", pair, replacement);
+        println!("Replace '{:?}' -> {:?} ", pair, char::from(replacement));
     }
 }
 
@@ -135,7 +128,20 @@ mod tests {
 
     #[test]
     fn test_replace() {
-        let (res, _count) = replace(b"AA", Pair(b'A', b'A'), b'B');
+        let res = replace(b"AA", Pair(b'A', b'A'), b'B');
         assert_eq!(res, vec![b'B']);
+    }
+
+    #[test]
+    fn test_compress() {
+        let (out, table) = compress("He Hello World AAA XYZ");
+        assert_eq!(
+            table,
+            vec![
+                (Pair(b'H', b'e'), b'B'), //
+                (Pair(b'A', b'A'), b'C'),
+            ]
+        );
+        assert_eq!(out, "B Bllo World CA XYZ".as_bytes());
     }
 }
