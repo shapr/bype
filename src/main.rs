91b7e00083a68fafd51e3865da0f7968e727279c
Shae Erisson <shae@scannedinavian.com>
2024-07-02 20:43:16 -0400

    pull loop out into a separate function

--- src/main.rs
+++ src/main.rs
@@ -7,49 +7,56 @@ fn replace(source: &[u8], replacement: Pair, id: u8) -> Vec<u8> {
     let mut skip_next = false;
 
     for pair in std::iter::zip(source.iter(), source[1..].iter()) {
-        if skip_next {
-            skip_next = false;
-            continue;
-        }
-        if (*pair.0, *pair.1) == replacement {
-            buffer.push(id);
-            skip_next = true;
-        } else {
-            buffer.push(*pair.0);
-        }
+	if skip_next {
+	    skip_next = false;
+	    continue;
+	}
+	if (*pair.0, *pair.1) == replacement {
+	    buffer.push(id);
+	    skip_next = true;
+	} else {
+	    buffer.push(*pair.0);
+	}
     }
     if !skip_next {
-        buffer.push(*source.last().unwrap());
+	buffer.push(*source.last().unwrap());
     }
     buffer
 }
 
 fn main() {
     let input = "he hello world AAAX";
-    let mut bytes: Vec<u8> = input.as_bytes().into();
+    let bytes: Vec<u8> = input.as_bytes().into();
+
+    let table = build_table(bytes);
+    for row in table {
+	println!(
+	    "{:?} replaces {:?} {:?}",
+	    char::from(row.1),
+	    char::from(row.0 .0),
+	    char::from(row.0 .1)
+	);
+    }
+}
+
+fn build_table(mut bytes: Vec<u8>) -> Vec<((u8, u8), u8)> {
     // A list of Replacements and Counts.
     // Pair = (a,a), Replacement = H
-    let mut table: Vec<(Pair, u8)> = Vec::new(); // Pair, Replacement
+    let mut table: Vec<(Pair, u8)> = Vec::new();
+    // Pair, Replacement
 
     while let Some(candidate_pair) = most_frequent_pair(&bytes) {
-        if let Some(unused_byte) = get_unused_byte(&bytes) {
-            println!("Replace {candidate_pair:?} with {unused_byte:?}");
+	if let Some(unused_byte) = get_unused_byte(&bytes) {
+	    println!("Replace {candidate_pair:?} with {unused_byte:?}");
 
-            table.push((candidate_pair, unused_byte));
+	    table.push((candidate_pair, unused_byte));
 
-            println!("BYTES BEFORE {:?}", String::from_utf8(bytes.clone()));
-            bytes = replace(&bytes, candidate_pair, unused_byte);
-            println!("BYTES AFTER {:?}", String::from_utf8(bytes.clone()));
-        }
+	    println!("BYTES BEFORE {:?}", String::from_utf8(bytes.clone()));
+	    bytes = replace(&bytes, candidate_pair, unused_byte);
+	    println!("BYTES AFTER {:?}", String::from_utf8(bytes.clone()));
+	}
     }
-    for row in table {
-        println!(
-            "{:?} replaces {:?} {:?}",
-            char::from(row.1),
-            char::from(row.0 .0),
-            char::from(row.0 .1)
-        );
-    }
+    table
 }
 
 fn most_frequent_pair(bytes: &[u8]) -> Option<Pair> {
@@ -57,14 +64,14 @@ fn most_frequent_pair(bytes: &[u8]) -> Option<Pair> {
 
     // Count pairs
     for pair in std::iter::zip(bytes.iter(), bytes[1..].iter()) {
-        let k = (*pair.0, *pair.1);
-        let count = *counts.get(&k).unwrap_or(&0);
-        counts.insert(k, count + 1);
+	let k = (*pair.0, *pair.1);
+	let count = *counts.get(&k).unwrap_or(&0);
+	counts.insert(k, count + 1);
     }
     counts
-        .iter()
-        .max_by(|a, b| a.1.cmp(b.1))
-        .and_then(|(pair, count)| if *count > 1 { Some(*pair) } else { None })
+	.iter()
+	.max_by(|a, b| a.1.cmp(b.1))
+	.and_then(|(pair, count)| if *count > 1 { Some(*pair) } else { None })
 }
 
 fn get_unused_byte(used_bytes: &[u8]) -> Option<u8> {
@@ -84,7 +91,7 @@ mod tests {
 
     #[test]
     fn test_replace() {
-        let res = replace(b"AA", (b'A', b'A'), b'B'); // cooool
-        assert_eq!(res, vec![b'B']);
+	let res = replace(b"AA", (b'A', b'A'), b'B'); // cooool
+	assert_eq!(res, vec![b'B']);
     }
 }
